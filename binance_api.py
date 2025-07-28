68f02df1ab2df582a5c09bd918dc19f0fc04bfd8
ThePositree <ThePositree@yandex.ru>
2025-07-28 14:23:43 +0300

    WIP

--- binance_api.py
+++ binance_api.py
@@ -235,6 +235,7 @@ class BinanceAPI:
             order_id = order_result['orderId']
             avg_price = float(order_result.get('avgPrice', 0))
             executed_qty = float(order_result.get('executedQty', 0))
+            print(order_result)
     
             self.logger.info(f"✅ Ордер открыт: {symbol_for_request} {binance_side} {executed_qty} по средней цене {avg_price}. Order ID: {order_id}")
     
